d9a53ac1635f053c2f100132598e460db464d5d3
ali <iamaliahad@outlook.com>
2023-01-24 04:56:29 +0800

    h-fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,9 @@
 export default function Header() {
   return (
-    <header className="z-40 items-center w-full h-16 bg-white shadow-lg">
+    <header className="z-40 items-center w-full h-16 bg-white border-2 border-gray-100 rounded-lg">
       <div className="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
         <div className="relative flex items-center w-full pl-1 lg:max-w-68 sm:pr-2 sm:ml-0">
-          <div className="container relative left-0 z-50 flex w-3/4 h-auto h-full">
-            
-          </div>
+          <div className="container relative left-0 z-50 flex w-3/4 h-auto h-full"></div>
           <div className="relative flex items-center justify-end w-1/4 p-1 ml-5 mr-4 sm:mr-0 sm:right-auto">
             <a href="https://www.aliahad.com/" className="relative block">
               <img
