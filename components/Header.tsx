4439955c28b370b89b40748635cccff1cbb80d6a
ali <iamaliahad@outlook.com>
2023-01-25 00:41:27 +0800

    professional mistaker

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,30 @@ export default function Header() {
     <header className="z-40 items-center w-full h-16 bg-white border-2 border-gray-100 rounded-lg">
       <div className="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
         <div className="relative flex items-center w-full pl-1 lg:max-w-68 sm:pr-2 sm:ml-0">
-          <div className="container relative left-0 z-50 flex w-3/4 h-auto h-full"></div>
+          <div className="container relative left-0 z-50 flex w-3/4 h-auto h-full">
+            <span className="text-sm text-gray-500 sm:text-center pr-5">
+              {" "}
+              Join communities to get update my next project -
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+              <li>
+                <a
+                  href="https://www.facebook.com/groups/aicommunity1/"
+                  className="mr-4 hover:underline md:mr-6 "
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://t.me/aicommunity2"
+                  className="mr-4 hover:underline md:mr-6"
+                >
+                  Telegram
+                </a>
+              </li>
+            </ul>
+          </div>
           <div className="relative flex items-center justify-end w-1/4 p-1 ml-5 mr-4 sm:mr-0 sm:right-auto">
             <a href="https://www.aliahad.com/" className="relative block">
               <img
